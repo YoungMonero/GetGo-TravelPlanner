848c766c83878ed7384623648ef07f9c93dbe270
youngmonero <yungmonero29@gmail.com>
2026-05-18 12:37:12 +0100

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 // Create a React Query client
 const queryClient = new QueryClient();
